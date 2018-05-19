8bc3e7d3fa8105d921257356866d09b9f76eea06
Jorge Ruiz <jruiz1987@gmail.com>
2018-05-20 00:42:23 +0200

    Update locationbot.py

--- locationbot.py
+++ locationbot.py
@@ -6,7 +6,19 @@ bot = telebot.TeleBot('593925092:AAENICMP76F9pQhfuOy1U92QNK0srb6FVvI')
 @bot.message_handler(content_types=['text'])
 def handle_text(mensaje):
   id_chat= mensaje.chat.id
-  localizacion = mensaje.text
+  location = mensaje.text
+  URL = "http://maps.googleapis.com/maps/api/geocode/json"
+  PARAMS = {'address':location}
+  r = requests.get(url = URL, params = PARAMS)
+  # extracting data in json format
+  data = r.json()
+  # extracting latitude, longitude and formatted address 
+  #of the first matching location
+  latitude = data['results'][0]['geometry']['location']['lat']
+  longitude = data['results'][0]['geometry']['location']['lng']
+  formatted_address = data['results'][0]['formatted_address']
+  #printing the output
+  localizacion ="Latitude:%s\nLongitude:%s\nFormatted Address:%s" %(latitude, longitude,formatted_address)
   bot.send_message(id_chat, localizacion)
  
 
