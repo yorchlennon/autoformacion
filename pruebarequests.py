03cee9991d7c08643395daa2f18e42b2406343a8
Jorge Ruiz <jruiz1987@gmail.com>
2018-05-19 03:41:43 +0200

    Create pruebarequests.py

---
+++ pruebarequests.py
@@ -0,0 +1,6 @@
+import requests
+req = requests.post('https://en.wikipedia.org/w/index.php', data = {'search':'Nanotechnology'})
+req.raise_for_status()
+with open('Nanotechnology.html', 'wb') as fd:
+    for chunk in req.iter_content(chunk_size=50000):
+        fd.write(chunk)
