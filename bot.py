2b4bbe4deda818b77c489cc903a872b30411e4ea
Jorge Ruiz <jruiz1987@gmail.com>
2018-05-19 02:30:09 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import telebot 
+import random
+bot = telebot.TeleBot('538628418:AAFjQ3IGb0c2neSjAWO8xb5e0n1w1l2NN28')
+@bot.message_handler(commands=['saluda'])
+def saluda(mensaje):
+     id_chat= mensaje.chat.id #El id del chat para saber el destino de la respuesta que va a enviar el bot
+     bot.send_message( id_chat, 'Que pasa gente') 
+@bot.message_handler(commands=['planes'])
+def planes(mensaje):
+    plan =  ['¿Cuando vamos a la Warner que tengo 2x1?','Tengo barbacoa en el Chaparral','Ya no propongo mas planecillos','¿Está Ovi?','Estoy en Salou']
+    aleatorio = random.randrange(0,len(plan),1)
+    id_chat= mensaje.chat.id 
+    bot.send_message( id_chat, plan[aleatorio])   
+@bot.message_handler(commands=['selfie'])
+def selfie(mensaje):
+    id_chat = mensaje.chat.id
+    bot.send_photo(id_chat, photo=open('rattata.jpg', 'rb'))
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception as e:
+        logger.error(e)
+        time.sleep(15)
